ce7f7a209c2f31c34d44a0c0398b941273bb1df7
kunal deepak patil <132162026+kunalpro379@users.noreply.github.com>
2024-10-21 13:56:27 +0530

    Update application.py

--- application.py
+++ application.py
@@ -95,5 +95,6 @@ def events_get():
     return jsonify({'events': events_to_execute})
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))
-    application.run(host='0.0.0.0', port=port, debug=True)
+    #port = int(os.environ.get("PORT", 5000))
+    #application.run(host='0.0.0.0', port=port, debug=True)
+    application.run(debug=True)
